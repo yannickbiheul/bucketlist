887ba029c61ddd069225336fb7a730aabff3c5a7
Yannick Biheul <yannick.biheul2022@campus-eni.fr>
2022-06-01 12:17:58 +0200

    balek

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,20 +12,26 @@ class WishController extends AbstractController {
     /**
      * @Route("/list", name="app_list")
      */
-    public function list():Response {
+    public function list(WishRepository $wishRepository):Response {
+        $wishs = $wishRepository->findAll();
+        dump($wishs);
         $titrePage = "List";
         return $this->render("wish/list.html.twig", [
-            'titre' => $titrePage
+            'titre' => $titrePage,
+            'wishs' => $wishs
         ]);
     }
 
     /**
-     * @Route("/detail", name="app_detail")
+     * @Route("/detail/{id}", name="app_detail", 
+     * requirements={"id"="\d+"}, methods={"GET"})
      */
-    public function detail():Response {
+    public function detail(WishRepository $wishRepository, $id):Response {
+        $wish = $wishRepository->findBy($id);
         $titrePage = "Detail";
         return $this->render("wish/detail.html.twig", [
-            'titre' => $titrePage
+            'titre' => $titrePage,
+            'id' => $id
         ]);
     }
 }
