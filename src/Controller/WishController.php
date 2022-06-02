2020d11c8797a1dc92650c008a78c55d1d5134db
Yannick Biheul <yannick.biheul2022@campus-eni.fr>
2022-06-02 16:46:50 +0200

    Ajout formulaire

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,19 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
 use App\Repository\WishRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class WishController extends AbstractController {
+class WishController extends AbstractController
+{
 
     /**
      * @Route("/list", name="app_list")
      */
-    public function list(WishRepository $wishRepository):Response {
+    public function list(WishRepository $wishRepository): Response
+    {
         $wishs = $wishRepository->findAll();
-        dump($wishs);
         $titrePage = "List";
         return $this->render("wish/list.html.twig", [
             'titre' => $titrePage,
@@ -26,12 +29,35 @@ class WishController extends AbstractController {
      * @Route("/detail/{id}", name="app_detail", 
      * requirements={"id"="\d+"}, methods={"GET"})
      */
-    public function detail(WishRepository $wishRepository, $id):Response {
-        $wish = $wishRepository->findBy($id);
+    public function detail(WishRepository $wishRepository, $id): Response
+    {
+        $wish = $wishRepository->find($id);
         $titrePage = "Detail";
         return $this->render("wish/detail.html.twig", [
             'titre' => $titrePage,
             'id' => $id
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="app_create")
+     */
+    public function create(Request $request, WishRepository $wishRepo)
+    {
+        if ($request->request->get('title') != null) {
+            $wish = new Wish();
+            $wish->setTitle($request->request->get('title'));
+            $wish->setDescription($request->request->get('description'));
+            $wish->setAuthor($request->request->get('author'));
+            $wishRepo->add($wish, true);
+
+            return $this->redirectToRoute('app_list');
+        }
+
+
+        $titrePage = "Create Wish";
+        return $this->render("wish/create.html.twig", [
+            'titre' => $titrePage,
         ]);
     }
 }
